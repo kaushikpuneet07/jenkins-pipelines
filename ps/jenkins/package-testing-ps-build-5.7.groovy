6cb9c5ea9b16932042cce1ed2b0bf3a8819be392
Puneet Kaushik <puneet.kaushik@percona.com>
2022-08-09 13:46:44 +0530

    package testing without mysql shell

--- ps/jenkins/package-testing-ps-build-5.7.groovy
+++ ps/jenkins/package-testing-ps-build-5.7.groovy
@@ -103,7 +103,7 @@ void runPlaybook(String action_to_test) {
     setup_package_tests()
 
     sh '''
-        git clone --depth 1 https://github.com/Percona-QA/package-testing
+        git clone --depth 1 --branch 5.7.test https://github.com/kaushikpuneet07/package-testing
     '''
 
     sh """
