a0cf783925b52626c393fc39fe5ef6850d6b739d
Puneet Kaushik <puneet.kaushik@percona.com>
2022-11-23 15:51:08 +0530

    5.7 test

--- ps/jenkins/package-testing-ps-build-5.7.groovy
+++ ps/jenkins/package-testing-ps-build-5.7.groovy
@@ -1,6 +1,6 @@
 library changelog: false, identifier: 'lib@master', retriever: modernSCM([
     $class: 'GitSCMSource',
-    remote: 'https://github.com/Percona-Lab/jenkins-pipelines.git'
+    remote: 'https://github.com/kaushikpuneet07/jenkins-pipelines.git'
 ]) _
 
 setup_rhel_package_tests = { ->
@@ -105,7 +105,7 @@ void runPlaybook(String action_to_test) {
     setup_package_tests()
 
     sh '''
-        git clone --depth 1 https://github.com/Percona-QA/package-testing
+        git clone --depth 1 -b 5.7.test https://github.com/kaushikpuneet07/package-testing
     '''
 
     sh """
