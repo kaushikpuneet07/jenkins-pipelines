12ac2f14f1657808d12c685e22b90fb5e079021c
Puneet Kaushik <puneet.kaushik@percona.com>
2023-02-07 11:32:22 +0530

    debug kmip playbook

--- ps/jenkins/package-testing-ps-build-8.0.groovy
+++ ps/jenkins/package-testing-ps-build-8.0.groovy
@@ -1,6 +1,6 @@
 library changelog: false, identifier: 'lib@master', retriever: modernSCM([
     $class: 'GitSCMSource',
-    remote: 'https://github.com/Percona-Lab/jenkins-pipelines.git'
+    remote: 'https://github.com/kaushikpuneet07/jenkins-pipelines.git'
 ]) _
 
 setup_rhel_package_tests = { ->
@@ -102,7 +102,7 @@ void runPlaybook(String action_to_test) {
     setup_package_tests()
 
     sh '''
-        git clone --depth 1 https://github.com/Percona-QA/package-testing
+        git clone -b kmip-fix --depth 1 https://github.com/kaushikpuneet07/package-testing
     '''
 
     sh """
