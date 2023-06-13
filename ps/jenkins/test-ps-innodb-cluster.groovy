1c151b7889cab31383c241a6bc9f47b39e331891
Puneet Kaushik <puneet.kaushik@percona.com>
2023-06-13 16:28:55 +0530

    Testing bionic distribution

--- ps/jenkins/test-ps-innodb-cluster.groovy
+++ ps/jenkins/test-ps-innodb-cluster.groovy
@@ -1,6 +1,6 @@
-library changelog: false, identifier: 'lib@master', retriever: modernSCM([
+library changelog: false, identifier: 'lib@bio', retriever: modernSCM([
     $class: 'GitSCMSource',
-    remote: 'https://github.com/Percona-Lab/jenkins-pipelines.git'
+    remote: 'https://github.com/kaushikpuneet07/jenkins-pipelines.git'
 ]) _
 
 void installDependencies() {
@@ -16,7 +16,7 @@ void installDependencies() {
 
     sh '''
         rm -rf package-testing
-        git clone https://github.com/Percona-QA/package-testing
+        git clone -b bio https://github.com/kaushikpuneet07/package-testing
     '''
 }
 
