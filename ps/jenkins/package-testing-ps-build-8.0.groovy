c1168cedc33d7a9d1cdad86db8ac86cef40832f0
Puneet Kaushik <puneet.kaushik@percona.com>
2023-09-22 13:03:33 +0530

    Testing AL2

--- ps/jenkins/package-testing-ps-build-8.0.groovy
+++ ps/jenkins/package-testing-ps-build-8.0.groovy
@@ -1,6 +1,6 @@
-library changelog: false, identifier: 'lib@master', retriever: modernSCM([
+library changelog: false, identifier: 'lib@py-al2', retriever: modernSCM([
     $class: 'GitSCMSource',
-    remote: 'https://github.com/Percona-Lab/jenkins-pipelines.git'
+    remote: 'https://github.com/kaushikpuneet07/jenkins-pipelines.git'
 ]) _
 
 setup_rhel_package_tests = { ->
@@ -111,7 +111,7 @@ void runPlaybook(String action_to_test) {
     setup_package_tests()
 
     sh '''
-        git clone --depth 1 https://github.com/Percona-QA/package-testing
+        git clone -b py-al2 --depth 1 https://github.com/kaushikpuneet07/package-testing
     '''
 
     sh """
