1f09d10511f6b2c7f9ccb69f0cf774496b08a07c
Puneet Kaushik <puneet.kaushik@percona.com>
2024-03-27 14:43:30 -0400

    Fix kmip

--- ps/jenkins/package-testing-ps-build-innovation-lts.groovy
+++ ps/jenkins/package-testing-ps-build-innovation-lts.groovy
@@ -1,6 +1,6 @@
-library changelog: false, identifier: 'lib@master', retriever: modernSCM([
+library changelog: false, identifier: 'lib@kmip-issue', retriever: modernSCM([
     $class: 'GitSCMSource',
-    remote: 'https://github.com/Percona-Lab/jenkins-pipelines.git'
+    remote: 'https://github.com/kaushikpuneet07/jenkins-pipelines.git'
 ]) _
 
 setup_rhel_package_tests = { ->
@@ -119,7 +119,7 @@ void runPlaybook(String action_to_test) {
     setup_package_tests()
 
     sh '''
-        git clone -b master --depth 1 https://github.com/Percona-QA/package-testing
+        git clone -b kmip-issue --depth 1 https://github.com/kaushikpuneet07/package-testing
     '''
 
     sh """
@@ -296,3 +296,4 @@ pipeline {
         }
     }
 }
+
